7be5697bc68b8d78378d0343da4e110a04be08e1
Jonathan Mendoza <quartet4@mac.com>
2022-08-25 21:12:38 -0400

    add menu functions

--- HabitTracker.cs
+++ HabitTracker.cs
@@ -2,7 +2,7 @@
 
 namespace Habit_Tracker;
 
-class HabitTracker
+static class HabitTracker
 {
     static readonly string ConnectionString = @"Data Source=habit-Tracker.db";
 
@@ -33,15 +33,142 @@ class HabitTracker
 
         /* Once we check if the database exists and create it (or not),
         we will call the next method, which will handle the user's input. Your next step is to create this method*/
-        GetUserInput();
+        bool keepGoing = true;
+
+        while (keepGoing)
+        {
+            keepGoing = GetUserInput();
+        }
     }
 
-    private static void GetUserInput()
+    private static bool GetUserInput()
     {
-        Console.ReadKey();
+        int userChoice;
+        
+        Console.WriteLine("----------------");
+        Console.WriteLine("0 - Quit");
+        Console.WriteLine("1 - View All");
+        Console.WriteLine("2 - Insert");
+        Console.WriteLine("3 - Delete");
+        Console.WriteLine("4 - Update");
+        Console.WriteLine("----------------");
+        
+        do
+        {
+            Console.Write("> ");
+        } while (!int.TryParse(Console.ReadLine(), out userChoice));
+
+        if (userChoice == 0)
+        {
+            Console.WriteLine("Quitting...");
+            return false;
+        }
+
+        
+        RunCommand(userChoice);
+        return true;
     }
-    
-    static void Main()
+
+    private static void RunCommand(int commandChoice)
+    {
+        using var connection = new SqliteConnection("Data Source=yourHabit.db");
+        connection.Open();
+
+        var command = connection.CreateCommand();
+
+        switch (commandChoice)
+        {
+            case 1:
+                command.CommandText =
+                    @"SELECT *
+                    FROM yourHabit
+                    ";
+                ViewAll(command);
+                break;
+            case 2:
+                Console.Write("Enter date: ");
+                string? date = Console.ReadLine();
+                Console.Write("Enter quantity: ");
+                int quantity = Convert.ToInt32(Console.ReadLine());
+                command.CommandText =
+                    @"INSERT INTO yourHabit (Date,Quantity)
+                    VALUES (@date,@qty)
+                    ";
+                command.Parameters.AddWithValue("@date", date);
+                command.Parameters.AddWithValue("@qty", quantity);
+                using (var _ = command.ExecuteReader())
+                {
+                    Console.WriteLine("Insert successful.");
+                }
+                break;
+            case 3:
+                command.CommandText =
+                    @"SELECT *
+                    FROM yourHabit
+                    ";
+                ViewAll(command);
+                Console.Write("Enter Id of record to delete: ");
+                int id = Convert.ToInt32(Console.ReadLine());
+                command.CommandText =
+                    @"DELETE FROM yourHabit
+                    WHERE Id =@id
+                    ";
+                command.Parameters.AddWithValue("@id", id);
+                using (var _ = command.ExecuteReader())
+                {
+                    Console.WriteLine("Delete successful.");
+                }
+                break;
+            case 4:
+                command.CommandText =
+                    @"
+                        SELECT *
+                        FROM yourHabit
+                        ";
+                ViewAll(command);
+                Console.Write("Enter Id of record to update: ");
+                int id2 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter new quantity: ");
+                int qty = Convert.ToInt32(Console.ReadLine());
+                command.CommandText =
+                    @"UPDATE yourHabit
+                    SET Quantity = @qty
+                    WHERE Id = @id;
+                    ";
+                command.Parameters.AddWithValue("@id", id2);
+                command.Parameters.AddWithValue("@qty", qty);
+                using (var _ = command.ExecuteReader())
+                {
+                    Console.WriteLine("Update successful.");
+                }
+                break;
+            default:
+                command.CommandText =
+                    @"
+                        SELECT *
+                        FROM yourHabit
+                        ";
+                //command.Parameters.AddWithValue("$id", id);
+                break;
+        }
+    }
+
+    private static void ViewAll(SqliteCommand command)
+    {
+        using var reader = command.ExecuteReader();
+        Console.WriteLine($"{"ID",-4}{"DATE",-11}{"QUANTITY",-14}");
+        while (reader.Read())
+        {
+            var id = reader.GetString(0);
+            var date = reader.GetString(1);
+            var quantity = reader.GetString(2);
+                    
+            Console.WriteLine($"{id,-4}{date,-11} {quantity,-14}");
+        }
+        Console.WriteLine();
+    }
+
+    private static void Main()
     {
         HabitTracker.CreateDatabase();
     }
